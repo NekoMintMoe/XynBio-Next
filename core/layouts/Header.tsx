2f31fb8e17c2d2f905df0f36cba026da75ae57b3
Season Shi <seasonshi@elyart.dev>
2023-02-08 01:14:56 +0800

    Add some color

--- core/layouts/Header.tsx
+++ core/layouts/Header.tsx
@@ -4,9 +4,9 @@ const Header = () => {
     return <header>
       <div>
         <Link href="/">
-          <p className="text-3xl font-bold">XynBio Next</p>
+          <p className="text-3xl font-bold text-pink-400">XynBio Next</p>
         </Link>
-        <p className="text-2xl">NextJS!</p>
+        <p className="text-2xl font-bold text-blue-400">NextJS!</p>
         <br />
       </div>
     </header>
