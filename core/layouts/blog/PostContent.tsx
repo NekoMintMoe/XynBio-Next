1434ba48f651c8b696570e35b2cfcf44b2fdd402
Season Shi <seasonshi@elyart.dev>
2023-02-08 01:57:49 +0800

    Fix Markdown Style

--- core/layouts/blog/PostContent.tsx
+++ core/layouts/blog/PostContent.tsx
@@ -3,8 +3,10 @@ import Markdown from 'markdown-to-jsx'
 const PostContent = (props: any) => {
     return (
         <div>
-            <h1>{props.post.data.title}</h1>
-            <Markdown>{props.post.content}</Markdown>
+            <p className="text-3xl text-lime-500 font-bold">{props.post.data.title}</p>
+            <article className="prose lg:prose-xl">
+                <Markdown>{props.post.content}</Markdown>
+            </article>
         </div>
     )
 }
