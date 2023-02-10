81864b3410af8ac6c3139342e36955bbb768df06
Season Shi <seasonshi@elyart.dev>
2023-02-11 01:24:45 +0800

    Add header to axios process

--- core/functions/blog/PostContent.ts
+++ core/functions/blog/PostContent.ts
@@ -1,8 +1,12 @@
 import { ContentData } from "@/core/modules/BlogData"
+import { DefaultHeader } from "@/core/modules/Headers"
 import axios from "axios"
 
 const DataAPI = async (slug: string): Promise<ContentData> => {
-    const res = await (await axios.get(`${process.env.DATA_API_URL}/api/blog/${slug}`)).data
+    const res = await (await axios(`${process.env.DATA_API_URL}/api/blog/${slug}`, {
+        method: "GET",
+        headers: DefaultHeader(),
+    })).data
     return {
         code: res.code,
         data: res.data,
--- core/functions/blog/PostList.ts
+++ core/functions/blog/PostList.ts
@@ -1,8 +1,13 @@
 import { ListData } from "@/core/modules/BlogData"
 import axios from "axios"
+import { DefaultHeader } from "@/core/modules/Headers"
 
 const DataAPI = async (): Promise<ListData> => {
-    const res = await (await axios.get(`${process.env.DATA_API_URL}/api/blog`)).data
+    
+    const res = await (await axios(`${process.env.DATA_API_URL}/api/blog`, {
+        method: "GET",
+        headers: DefaultHeader(),
+    })).data
     return {
         code: res.code,
         data: res.data,
