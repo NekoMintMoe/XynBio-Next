c0bb5557196ce4fac7d15b6a8865bd268a1d1473
Season Shi <seasonshi@elyart.dev>
2023-02-18 18:34:38 +0800

    async for callback api

--- pages/api/callback.ts
+++ pages/api/callback.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== "GET") {
         return res.status(405).json({ message: "Method Not Allowed" });
     }
